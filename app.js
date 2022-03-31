a51683019cbaecec436f82147ed5039b64062d63
shukrullah shirzad <shukrullah.shirzad.779@gmail.com>
2022-03-31 14:26:51 +0430

    added the timer circle

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const start = document.querySelector(".fa-play");
 const pause = document.querySelector(".fa-pause");
-
-
+let circle = document.querySelector('#circle');
+function animate(){
+  circle.style.transition = "animate 9s linear forwards"
+}
+console.log(circle)
 start.addEventListener("click",(e)=>{
   pause.classList.remove("hide");
   start.classList.add("hide");
+  animate();
 })
 pause.addEventListener("click",(e)=>{
   pause.classList.add("hide");
