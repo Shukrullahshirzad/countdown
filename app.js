b2a611e026491cdd8aadd803a34ccd511b2e5a0b
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2022-04-10 14:38:46 +0430

    create variables for hour, minutes and seconds

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const pause = document.querySelector(".fa-pause");
 let circle = document.querySelector('#circle');
 const timer = document.querySelector('.time-display');
 const timeInput = document.querySelector('.time-input');
+// user input for time
 let hourInput = document.querySelector('.hour-in');
 let minsInput = document.querySelector('.mins-in');
 let secsInput = document.querySelector('.secs-in');
+console.log(hourInput.value*3600, +minsInput.value*60, +secsInput.value);
+// total time
+let totalTime = (+hourInput.value)*3600 + (+minsInput.value) * 60 + (+secsInput.value);
+console.log(totalTime)
+// time in hour, mins and secs
+let hoursDisplay = totalTime / 3600;
+console.log(hoursDisplay)
+let minutesDisplay = hoursDisplay / 60;
+let secondsDisplay = minutesDisplay / 60; 
 let hourMins = document.querySelector('.hour-mins');
 let secs = document.querySelector('.secs');
-hourMins.textContent = hourInput.value + ":" + minsInput.value;
+hourMins.textContent = totalTime/hoursDisplay + ":" ;
+
 secs.textContent = secsInput.value;
+
+function reduceTime(time){
+  time -= 1
+}
 start.addEventListener("click",(e)=>{
   pause.classList.remove("hide");
   start.classList.add("hide");
