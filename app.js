81cbb664ee015602e997312389cd52c62cb69615
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2022-04-10 13:55:11 +0430

    add time count-down

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const start = document.querySelector(".fa-play");
 const pause = document.querySelector(".fa-pause");
 let circle = document.querySelector('#circle');
-function animate(){
-  circle.style.transition = "animate 9s linear forwards"
-}
-console.log(circle)
+const timer = document.querySelector('.time-display');
+const timeInput = document.querySelector('.time-input');
+let hourInput = document.querySelector('.hour-in');
+let minsInput = document.querySelector('.mins-in');
+let secsInput = document.querySelector('.secs-in');
+let hourMins = document.querySelector('.hour-mins');
+let secs = document.querySelector('.secs');
+hourMins.textContent = hourInput.value + ":" + minsInput.value;
+secs.textContent = secsInput.value;
 start.addEventListener("click",(e)=>{
   pause.classList.remove("hide");
   start.classList.add("hide");
-  animate();
+  timeInput.classList.add('hide');
+  timer.classList.remove('hide');
 })
 pause.addEventListener("click",(e)=>{
   pause.classList.add("hide");
